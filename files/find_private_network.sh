6989d8b397e7685aba7e3a2fc9d9d5f009dd0f74
Dawa Ometto <d.l.a.ometto1@uu.nl>
2026-08-06 14:26:37 +0200

    Fix shell script

--- files/find_private_network.sh
+++ files/find_private_network.sh
@@ -1,8 +1,10 @@
-#/bin/bash
+#!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
 
-result=$(ip -j addr | jq -r '.[] | select(.addr_info[]?.local | test("^(10\\.|172\\.(1[6-9]|2[0-9]|3[0-1])\\.|192\\.168\\.)")) | .ifname')
+# this command will return the .add_info[0].local attribute of any interface that matches a local network ip4
+# this ip could be either the ip4 or ip6 address -- we don't much care which we use.
+result=$(ip -j addr | jq -r '.[] | select(.addr_info[]?.local | test("^(10\\.|172\\.(1[6-9]|2[0-9]|3[0-1])\\.|192\\.168\\.)")) | .addr_info[0].local')
 
 if [[ -z "$result" ]]; then
     echo "No private network found"
